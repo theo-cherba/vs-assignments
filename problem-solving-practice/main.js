e78464a9d34d661e268b8c39a83041cec08db222
Theo Cherba <theo.cherba@gmail.com>
2021-10-19 13:52:38 -0700

    completed problem-solving-practice.

---
+++ problem-solving-practice/main.js
@@ -0,0 +1,28 @@
+// function largest(a) {
+//     let largest = Math.max(...a)
+//     return largest
+// }
+
+// arrayExample = [1, 3, 42, 10, 27]
+// console.log(largest(arrayExample))
+
+// function lettersWithStrings(a,b) {
+//     let result = a.filter(item => item.includes(b));
+//     console.log(result);
+// }
+
+// lettersWithStrings(["#3", "$$$", "C%4!", "Hey!"], "!")
+
+// function isDivisible(a, b) {
+//     if (a % b === 0) {
+//         console.log("true")
+//     } else {
+//         console.log("false")
+//     }   
+// }
+
+// isDivisible(4, 2) // => true
+// isDivisible(9, 3) // => true
+// isDivisible(15, 4) // => false
+
+console.log("GLHF")
